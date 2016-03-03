8b21d0e5a13014b6897a41fce6489262c6394fc9
Matthew Gates <matthewg42@gmail.com>
2016-03-03 22:42:25 +0000

    Swap to using argparse subcommands (add, list)
    Not everything implemented yet, but a template for the other
    functions too. Can delete home brew command parser when we're
    done.

--- lib/mpp/manager.py
+++ lib/mpp/manager.py
@@ -3,7 +3,6 @@ import os
 import logging
 from prettytable import PrettyTable
 from mpp.podcast import Podcast
-from mpp.cmdparser import CmdParser
 
 log = logging
 
@@ -12,17 +11,9 @@ class PodcastManager():
         self.config = config
         self.podcasts = []
         self.load_podcasts()
-        self.cmds = CmdParser()
-        self.cmds.register('add', self.add_podcast, 
-            'usage: add url [title]\nAdd a new podcast')
-        self.cmds.register('rm', self.remove_podcast, 
-            'usage: rm vilter\nRemove one or more podcasts')
-        self.cmds.register('ls', self.list_podcasts, 
-            'usage: ls [filter]\nShow a list of podcasts')
-        self.cmds.register('catchup', self.catchup_podcast, 
-            'usage: catchup filter [leave]\nCatch up specific episodes')
-        self.cmds.register('rename', self.rename_podcast, 
-            'usage: rename filter "new name"\nSet the title of the podcast')
+
+    def exec(self, args):
+        getattr(self, args.cmd)(args)
         
     def load_podcasts(self):
         log.debug('looking for feeds in: %s' % self.config['feed_dir'])
@@ -38,28 +29,28 @@ class PodcastManager():
     def get_podcast_path(self, p):
         return self.config['feed_dir'] + '/%s.json' % p.url_hash()
 
-    def add_podcast(self, url, title=None):
+    def add_podcast(self, args):
         # check if we already have it
-        p = Podcast(url)
+        p = Podcast(args.url)
         path = self.get_podcast_path(p)
         if os.path.exists(path):
-            raise(Exception('already exists: %s : %s' % (url, path)))
-        p = Podcast.from_url(url)
-        if title is not None:
-            p.title = title
+            raise(Exception('already exists: %s : %s' % (args.url, path)))
+        p = Podcast.from_url(args.url)
+        if args.title:
+            p.title = args.title
         self.podcasts.append(p)
         p.save_to_file(path)
         return p
 
     def remove_podcast(self, filter=None):
-        for p in [x for x in self.podcasts if x.matches_filter(filter)]:
+        for p in [x for x in self.podcasts if x.matches_filter(args.filter)]:
             p.delete_file()
 
-    def list_podcasts(self, filter=None):
-        log.debug('list_podcasts(%s)' % filter)
+    def list_podcasts(self, args):
+        log.debug('list_podcasts(%s)' % args.filter)
         t = PrettyTable()
         t.field_names = ['Title', 'File', '#Ep', '#New', '#Rdy', '#Done']
-        for p in [x for x in self.podcasts if x.matches_filter(filter)]:
+        for p in [x for x in self.podcasts if x.matches_filter(args.filter)]:
             t.add_row([ p.title,
                 p.url_hash() + '.json',
                 len(p.episodes), 
